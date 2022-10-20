203f35d4766e4c79c070257e3cf325aff4e54638
alopalka <d3bian@mopalka.idl.pl>
2022-10-20 22:22:41 +0200

    Added few tasks

--- JavaCodingBat/src/codingbat/warmup2/Main.java
+++ JavaCodingBat/src/codingbat/warmup2/Main.java
@@ -5,11 +5,44 @@ public class Main {
 
     }
 
-    public String stringTimes(String str,int n){
-        String varable= "";
+    public String stringTimes(String str, int n) {
+        String varable = "";
         for (int i = 0; i < n; i++) {
-            varable+=str;
+            varable += str;
         }
         return varable;
     }
+
+    public String frontTimes(String str, int n) {
+        String result = "";
+        for (int i = 0; i < n; i++) {
+            result += str.substring(0, Math.min(str.length(), 3));
+        }
+        return result;
+    }
+
+    public int countXX(String str) {
+        int counter = 0;
+
+        for (int i = 0; i < str.length() - 1; i++) {
+            if (str.substring(i, i + 2).equals("xx")) {
+                counter++;
+            }
+        }
+
+        return counter;
+    }
+
+    public boolean doubleX(String str) {
+        for (int i = 0; i < str.length() - 1; i++) {
+            if (str.charAt(i) == 'x') {
+                if (str.charAt(i+1) == 'x'){
+                    return true;
+                }
+                return false;
+            }
+        }
+        return false;
+    }
+
 }
