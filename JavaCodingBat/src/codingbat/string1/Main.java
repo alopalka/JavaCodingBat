ec61946685b10279d29dd3640bd512d5ece12d4c
alopalka <d3bian@mopalka.idl.pl>
2022-10-21 22:44:10 +0200

    Added few more completed tasks

--- JavaCodingBat/src/codingbat/string1/Main.java
+++ JavaCodingBat/src/codingbat/string1/Main.java
@@ -4,4 +4,18 @@ public class Main {
     public static void main(String[] args) {
 
     }
+
+    public String helloName(String name) {
+        return "Hello " + name + "!";
+    }
+
+    public String makeAbba(String a, String b) {
+        return a + b + b + a;
+    }
+
+    public String makeTags(String tag, String word) {
+        return "<" + tag + ">" + word + "</" + tag + ">";
+    }
+    
+
 }
--- JavaCodingBat/src/codingbat/warmup2/Main.java
+++ JavaCodingBat/src/codingbat/warmup2/Main.java
@@ -36,7 +36,7 @@ public class Main {
     public boolean doubleX(String str) {
         for (int i = 0; i < str.length() - 1; i++) {
             if (str.charAt(i) == 'x') {
-                if (str.charAt(i+1) == 'x'){
+                if (str.charAt(i + 1) == 'x') {
                     return true;
                 }
                 return false;
@@ -44,5 +44,33 @@ public class Main {
         }
         return false;
     }
+
+    public String stringBits(String str) {
+        String resultString = "";
+        for (int i = 0; i < str.length(); i += 2) {
+            resultString += str.charAt(i);
+        }
+        return resultString;
+    }
+
+    public String stringSplosion(String str) {
+        String resultString = "";
+        for (int i = 0; i < str.length(); i++) {
+            resultString += str.substring(0, i + 1);
+        }
+        return resultString;
+    }
+
+    public int last2(String str) {
+        int counter = 0;
+        String pattern = str.substring(0, 1);
+        for (int i = 0; i < str.length() - 1; i++) {
+            if (str.substring(i, i + 1).equals(pattern)) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+
 
 }
