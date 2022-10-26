deb1c93cfd13c634cd772fa69a4b257ab84bc680
Aleksander Opałka <d3bian@mopalka.idl.pl>
2022-10-26 22:33:52 +0200

    Created map package. Added few tasks

---
+++ JavaCodingBat/src/codingbat/map1/Main.java
@@ -0,0 +1,36 @@
+package codingbat.map1;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    public Map<String, String> mapBully(Map<String, String> map) {
+        if (map.containsKey("a")) {
+            map.put("b", map.get("a"));
+            map.put("a", "");
+        }
+        return map;
+    }
+
+    public Map<String, String> mapShare(Map<String, String> map) {
+        if (map.containsKey("a")) {
+            map.put("b", map.get("a"));
+        }
+        if (map.containsKey("c")) {
+            map.remove("c");
+        }
+        return map;
+    }
+
+    public Map<String, String> mapAB(Map<String, String> map) {
+        if (map.containsKey("a") && map.containsKey("b")) {
+            map.put("ab", map.get("a") + map.get("b"));
+        }
+        return map;
+    }
+
+}
