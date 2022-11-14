60761f25aec0cf8406d1758a11a3d5621579d59f
alopalka <d3bian@mopalka.idl.pl>
2022-11-14 18:09:44 +0100

    Minor change

--- src/codingbat/arrays2/ArraysTwo.java
+++ src/codingbat/arrays2/ArraysTwo.java
@@ -6,6 +6,12 @@ public class ArraysTwo {
     public static void main(String[] args) {
     }
 
+//    Return the number of even ints in the given array. Note: the % "mod" operator computes the remainder, e.g. 5 % 2 is 1.
+//
+//    countEvens([2, 1, 2, 3, 4]) → 3
+//    countEvens([2, 2, 0]) → 3
+//    countEvens([1, 3, 5]) → 0
+
     public int countEvens(List<Integer> nums) {
         return (int) nums.stream()
                 .filter(e -> e % 2 == 1)
